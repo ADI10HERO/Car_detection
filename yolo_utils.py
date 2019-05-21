47234444d76348c0132f9c6fc47987189c5b7764
Aditya Srivastava <adityasrivastava301199@gmail.com>
2019-05-21 11:22:38 +0530

    Updating stuff

--- yolo_utils.py
+++ yolo_utils.py
@@ -40,9 +40,14 @@ def scale_boxes(boxes, image_shape):
 
 def preprocess_image(img_path, model_image_size):
     image_type = imghdr.what(img_path)
+    print(image_type)
     image = Image.open(img_path)
+    #plt.imshow(np.array(temp)[:,:,:3])
+    
     resized_image = image.resize(tuple(reversed(model_image_size)), Image.BICUBIC)
-    image_data = np.array(resized_image, dtype='float32')
+    
+    image_data = np.array(resized_image, dtype= np.float32 )
+    image_data = image_data[:,:,:3]
     image_data /= 255.
     image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
     return image, image_data
